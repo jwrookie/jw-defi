9757d42ca9ad0f933e4f23032d06151a9e49a8b6
jw <a1@1deMacBook-Pro.local>
2022-07-07 13:38:53 +0800

    feat: config

--- hardhat.config.js
+++ hardhat.config.js
@@ -31,7 +31,7 @@ module.exports = {
     kccTest: {
       url: 'https://rpc-testnet.kcc.network',
       chainId: 322,
-      accounts: ['']
+      accounts: [process.env.PRIVATE_KEY]
       // gasPrice: 20000000000,
       // accounts: {mnemonic: mnemonic}
     },
@@ -41,7 +41,7 @@ module.exports = {
       from: "0x67F035CA9ba9eCE6A0cB068d7829ACA71cdd15A6",
       chainId: 3,
       // accounts 为账户私钥，可以添加多个
-      accounts : [""]
+      accounts : [process.env.PRIVATE_KEY]
     }
   },
   paths: {
@@ -56,7 +56,7 @@ module.exports = {
   etherscan: {
     apiKey: {
       // 以太坊测试网 的key，在主网上申请，他们通用
-      ropsten: '',
+      ropsten: process.env.ETHERSCAN_KEY,
       kccTest: ''
     },
     customChains: [
